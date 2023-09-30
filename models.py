aeb36f5f0c7552ffe27aecf77e55a19658bf4c7a
Tarmo Padam <tarmo@hotmail.com>
2023-09-30 13:45:59 +0300

    relatinships created

--- models.py
+++ models.py
@@ -1,8 +1,37 @@
-from sqlalchemy import create_engine, Column, ForeignKey, Integer, String, Boolean
+from sqlalchemy import create_engine, Table, Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base, relationship
 
 eng = create_engine('sqlite:///movies.db')
 Base = declarative_base()
+
+
+user_series = Table(
+    "user_series", 
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('series_id', Integer, ForeignKey('series.id')),
+ )
+
+user_movies = Table(
+    "user_movies", 
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('users.id')),
+    Column('movies_id', Integer, ForeignKey('movies.id')),
+ )
+
+watching_list_series = Table(
+    "watching_list_series", 
+    Base.metadata,
+    Column('watching_list_id', Integer, ForeignKey('watching_list_id')),
+    Column('series_id', Integer, ForeignKey('series.id')),
+)
+
+watching_list_movies = Table(
+    "watching_list_movies", 
+    Base.metadata,
+    Column('watching_list_id', Integer, ForeignKey('watching_list_id')),
+    Column('movies_id', Integer, ForeignKey('movies.id')),
+ )
 
 
 class Movie(Base):
@@ -12,6 +41,10 @@ class Movie(Base):
     title = Column(String(80), nullable=False)
     rating = Column(Integer, nullable=False)
     release_year = Column(String(4), nullable=False)
+
+    user = relationship("User", secondary=user_movies, back_populates="movies")
+    watchin_lists = relationship("WatchingList", secondary=watching_list_movies, back_populates="movies")
+
 
 
 class Series(Base):
@@ -23,6 +56,9 @@ class Series(Base):
     release_year = Column(String(4), nullable=False)
     amout_of_seasons = Column(Integer, nullable=False)
 
+    user = relationship("User", secondary=user_series, back_populates="series")
+    watchin_lists = relationship("WatchingList", secondary=watching_list_series, back_populates="series")
+
 
 class User(Base):
     __talename__ = 'user'
@@ -31,6 +67,12 @@ class User(Base):
     name = Column(String(20), nullable=False)
     surname = Column(String(20), default="")
 
+    watching_list = relationship("WatchingList", back_populates="user", cascade="all, delete-orphan")
+
+    movies = relationship("Movie", secondary=user_movies, back_populates="user")
+    series = relationship("Series", secondary=user_series, back_populates="user")
+
+
 
 class WatchingList(Base):
     __tablename__ = "watching_list"
@@ -38,6 +80,11 @@ class WatchingList(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(20), nullable=False)
     date_when_created = Column(String(20), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_id"))
+    user = relationship("User", back_populates="watch_lists")
+
+    movies = relationship("Movie", secondary=watching_list_movies, back_populates=""watching_lists")
+    series = relationship("Series", secondary=watching_list_series, back_populates="watching_lists")
 
 
 if __name__ == "__main__":
